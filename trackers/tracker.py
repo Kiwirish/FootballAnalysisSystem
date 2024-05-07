8c3035da0989f36d33b5d33cd543b1f2af189573
Blake Leahy <leabl393@student.otago.ac.nz>
2024-05-08 11:35:16 +1200

    Ball tracker implemented

--- trackers/tracker.py
+++ trackers/tracker.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import supervision as sv 
 import pickle
 import os
+import numpy as np
 import cv2
 import sys
 sys.path.append('../')
@@ -128,6 +129,24 @@ class Tracker:
         
         return frame
 
+    def draw_triangle(self, frame, bbox, color):
+
+        y = int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10,y-20],
+        
+        ])
+
+        cv2.drawContours(frame, [triangle_points],0,color,cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points],0,(0,0,0),2)
+
+        return frame 
+
+
 
     def draw_annotations(self, video_frames, tracks):
         output_video_frames = []
@@ -147,6 +166,10 @@ class Tracker:
             for _, referee in referee_dict.items():
 
                 frame = self.draw_ellipse(frame,referee["bbox"],(0,255,255))
+
+            # Draw ball 
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_triangle(frame,ball["bbox"],(0,255,0))
 
             output_video_frames.append(frame)
 
